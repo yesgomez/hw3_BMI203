fb7faef14ac0db00ee617d79027dc561b92986d4
Yessica Gomez <yessica.gomez@ucsf.edu>
2018-06-29 23:47:05 -0700

    Updated test script functions

--- test/test_align.py
+++ test/test_align.py
@@ -12,12 +12,14 @@ def test_import():
 
 def test_fpr():
 	# Can it calculate scores using different scoring matrices?
+	negpairlist_filename, pospairlist_filename, negpairs1, negpairs2, pospairs1, pospairs2, names, options = init_files(pwd, directory)	
 	all_score_mats(options, 4, 3, pospairs1, pospairs2, negpairs1, negpairs2, names) # hardcoded penalties for testing
 	print ("FPR test passed.\n")
 	
 def test_makeroc():
 	# Can it output an ROC curve using provided scores and scoring matrix?
-	fpr, pos_align_score, neg_align_score = single_scoring(each, 4, 3, pospairs1, pospairs2, negpairs1, negpairs2)
+	negpairlist_filename, pospairlist_filename, negpairs1, negpairs2, pospairs1, pospairs2, names, options = init_files(pwd, directory)	
+	fpr, pos_align_score, neg_align_score = single_scoring(options[0], 4, 3, pospairs1, pospairs2, negpairs1, negpairs2)
 	make_roc_curve(pos_align_score, neg_align_score, each, names[i])
 	print ("ROC curve test passed.\n")
 
